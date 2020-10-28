b9903a9ba951cf00c0f94573d7f264c38b2e6466
grigyan <grigornalbandyan26@gmail.com>
2020-10-28 21:31:02 +0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String args[]) {
+        char c = 'a';
+        System.out.println("char c = " + c);
+
+        boolean b = true;
+        System.out.println("boolean b = " + b);
+
+        byte b2 = 44;
+        System.out.println("byte b2 = " + b2);
+
+        short s = 34;
+        System.out.println("short s = " + s);
+
+        int a = 234;
+        System.out.println("int a = " + a);
+
+        long l = 4654L;
+        System.out.println("long l = " + l);
+
+        double d = 456.456d;
+        System.out.println("double d = " + d);
+
+        float f = 456.456f;
+        System.out.println("float f = " + f);
+    }
+}
